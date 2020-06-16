3c1265c8a506c36f25158f6832ae784fe43d1c40
Peter Gemmell <petergemmell65@hotmail.com>
2020-06-16 16:23:30 +0100

    account class created

--- server/bankservice/src/main/java/com/example/bankapp/bankservice/models/Account.java
+++ server/bankservice/src/main/java/com/example/bankapp/bankservice/models/Account.java
@@ -3,6 +3,8 @@ package com.example.bankapp.bankservice.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="accounts")
@@ -29,5 +31,75 @@ public class Account {
     @JoinColumn(name= "customer_id", nullable = false)
     private Customer customer;
 
+    @JsonIgnoreProperties(value="accounts")
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+    private List<Deposit> deposits;
 
+    public Account(String customerName, String accountType, String accountNo, Double currentBalance, Customer customer){
+        this.customerName = customerName;
+        this.accountType = accountType;
+        this.accountNo = accountNo;
+        this.currentBalance = currentBalance;
+        this.customer = customer;
+        this.deposits = new ArrayList<>();
+    }
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public Double getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(Double currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Deposit> getDeposits() {
+        return deposits;
+    }
+
+    public void setDeposits(List<Deposit> deposits) {
+        this.deposits = deposits;
+    }
 }
