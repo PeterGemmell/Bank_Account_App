203510e32430bc7c48819ef3cfffe346a04fcf63
Peter Gemmell <petergemmell65@hotmail.com>
2020-06-16 17:17:07 +0100

    deposit controller created

--- server/bankservice/src/main/java/com/example/bankapp/bankservice/controllers/DepositController.java
+++ server/bankservice/src/main/java/com/example/bankapp/bankservice/controllers/DepositController.java
@@ -1,4 +1,46 @@
 package com.example.bankapp.bankservice.controllers;
 
+import com.example.bankapp.bankservice.models.Deposit;
+import com.example.bankapp.bankservice.repositories.DepositRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class DepositController {
+
+    @Autowired
+    DepositRepository depositRepository;
+
+    @GetMapping(value = "/deposits")
+    public ResponseEntity<List<Deposit>> getAllDeposits(){
+        return new ResponseEntity<>(depositRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/deposits/{id}")
+    public ResponseEntity getDeposit(@PathVariable Long id){
+        return new ResponseEntity<>(depositRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/deposits")
+    public ResponseEntity<Deposit> postDeposit(@RequestBody Deposit deposit){
+        depositRepository.save(deposit);
+        return new ResponseEntity<>(deposit, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/deposits/{id")
+    public ResponseEntity<Deposit> updateDeposit(@RequestBody Deposit deposit){
+        depositRepository.save(deposit);
+        return new ResponseEntity<>(deposit, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/deposits/{id}")
+    public ResponseEntity<Deposit> deleteDeposit(@PathVariable Long id){
+        Deposit found = depositRepository.getOne(id);
+        depositRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
