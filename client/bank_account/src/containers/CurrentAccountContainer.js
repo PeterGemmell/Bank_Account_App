673532e8683ed9661ec690632310d54e10e61711
Peter Gemmell <petergemmell65@hotmail.com>
2020-06-24 11:14:05 +0100

    current account container created and current account list added

--- client/bank_account/src/containers/CurrentAccountContainer.js
+++ client/bank_account/src/containers/CurrentAccountContainer.js
@@ -1,33 +1,65 @@
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Request from '../helpers/request'
+// import CurrentAccountDetail from './components/CurrentAccountDetail';
+import CurrentAccountList from './components/CurrentAccountList';
 import NavBar from '../NavBar.js';
+
 
 class CurrentAccountContainer extends Component {
 
-render(){
-  return(
-    <Router>
-    <NavBar />
-    <Fragment>
-    <div className="card">
-      <h2>💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵</h2>
-      <h3>To view information on your accounts or to make a deposit, please select an option from the above menu.</h3>
-      <br>
-      </br>
-      <h5><b>Warm regards, G&A Online Bank</b></h5>
-    <div className="wrap">
-      <div className="left">
-        <form>
-         </form>
-       </div>
-    </div>
-    </div>
-    </Fragment>
-    </Router>
+ constructor(props) {
+   super(props);
+   this.state = {
+     accounts: []
+   }
 
-  )
-}
+   this.findAccountById = this.findAccountById.bind(this);
+ }
+
+ componentDidMount(){
+   const request = new Request();
+
+   request.get('/api/accounts')
+   .then((data) => {
+     this.setState({accounts: data})
+   })
+ }
+
+ // handlePost(account){
+ //   const request = new Request();
+ //   request.post('/api/accounts', account)
+ //   .then( () => {
+ //     window.location = '/accounts'
+ //   })
+ //  } // Not currently required as we are not making any edits to our account information.
+
+  findAccountById(id){
+    return this.state.accounts.find((account) => {
+      return account.id === parseInt(id);
+    });
+  }
+
+
+  render(){
+    if(!this.state.accounts){
+      return null
+    }
+
+    return(
+      <Router>
+      <NavBar/>
+      <Fragment>
+      <Switch>
+      <Route render = {(props) => {
+        return <CurrentAccountList accounts ={this.state.accounts} />
+      }}/>
+      </Switch>
+      </Fragment>
+      </Router>
+
+    )
+  }
 
 
 }
